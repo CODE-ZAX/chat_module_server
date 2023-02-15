7edce706f3a8fa966cf6b18328075d9e59999b74
CODE-ZAX <86313946+CODE-ZAX@users.noreply.github.com>
2023-02-15 12:39:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Middleware to parse request bodies as JSON
+app.use(express.json());
+
+
+// Route to handle chat messages
+app.post("/chat/:fid", (req, res) => {
+    const friendID = req.params.fid;
+    const fetchPreviousChat = []
+io.on("connection", (socket) => {
+  // Handle a new connection from a client
+  socket.on("chat-started", (userId) => {
+    io.emit("user-fetched", fetchPreviousChat);
+  });
+
+  // Handle a new chat message from a client
+  socket.on("chat message", ({ userId, message }) => {
+    const newMsg = {userId:userId,message:message};
+    fetchPreviousChat.push(newMsg);
+    io.emit("message", newMsg);
+  });
+
+  // Handle a disconnect event from a client
+  socket.on("disconnect", () => {
+    //store new chat
+    console.log("Client disconnected");
+  });
+});
+
+
+
+  // Broadcast the message to all connected clients
+  io.emit("message", { userId, message });
+});
+
+
+// Start the server
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
